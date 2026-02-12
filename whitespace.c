89b1fbd855777085af7662a3471f506c1ecc5503
MightyT-2 <timmightyt2@gmail.com>
2026-02-12 09:34:35 -0600

    Begin Whitespace Interpreter

---
+++ whitespace.c
@@ -0,0 +1,10 @@
+// Whitespace Interpreter
+// If no arguments are passed, it should act as a shell, similar to JShell or just typing Python in the terminal
+#include <stdio.h>
+
+
+
+int main(int argc, char* argv[])
+{
+    printf("Whitespace");
+}
