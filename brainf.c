544ee5f16d8cf6f0d07ade3c4c1a3a9ca2e80d36
MightyT-2 <timmightyt2@gmail.com>
2026-06-02 22:37:01 -0400

    Finish Parser and Interpreter

--- brainf.c
+++ brainf.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <termios.h>
 
 typedef struct cell
 {
@@ -10,15 +11,19 @@ typedef struct cell
 typedef struct instruct
 {
 	char instruct;
-	struct instruct *next_instruct;
-	struct instruct *matching_instruct;
+	struct instruct *p_next_instruct;
+	struct instruct *p_matching_instruct;
 } instruct;
+
+typedef struct termios termios;
 
 char get_next_token(FILE *source_file);
 
-instruct bf_parse(instruct *p_program, FILE *source_file);
+void bf_parse(instruct *p_program, FILE *source_file);
 
-void bf_run();
+void bf_run(instruct *p_program);
+
+cell *initialize_ribbon();
 
 int main()
 {
@@ -34,32 +39,106 @@ char get_next_token(FILE *source_file)
 	return next_char;
 }
 
-instruct bf_parse(instruct *p_program, FILE *source_file)
+void bf_parse(instruct *p_program, FILE *source_file)
 {
-	instruct **loop_stack;
-	instruct **curr_loop;
-	instruct *curr_instruct;
-	instruct *prev_instruct;
+	instruct **loop_stack = NULL;
+	int loop_stack_count = 0;
+	instruct *curr_instruct = NULL;
+	instruct *prev_instruct = p_program;
 	char next_char;
 
 	while ((next_char = get_next_token(source_file)) != EOF)
 	{
 		curr_instruct = calloc(sizeof(instruct), 1);
-		curr_instruct->next_instruct = prev_instruct->next_instruct;
-		prev_instruct->next_instruct = curr_instruct;
+		curr_instruct->instruct = next_char;
+		curr_instruct->p_next_instruct = prev_instruct->p_next_instruct;
+		prev_instruct->p_next_instruct = curr_instruct;
 		if (next_char == '[')
 		{
-
+			loop_stack = realloc(loop_stack, (++loop_stack_count) * sizeof(instruct *));
+			loop_stack[loop_stack_count - 1] = curr_instruct;
 		}
 		else if (next_char == ']')
 		{
-
-		}
-		else
-		{
-
+			if (loop_stack_count <= 0)
+			{
+				exit(1);
+			}
+			curr_instruct->p_matching_instruct = loop_stack[loop_stack_count];
+			curr_instruct->p_matching_instruct->p_matching_instruct = curr_instruct;
+			loop_stack = realloc(loop_stack, (--loop_stack_count) * sizeof(instruct *));
 		}
 		prev_instruct = curr_instruct;
 	}
 
+	if (loop_stack_count > 0)
+	{
+		exit(1);
+	}
+
+	return;
+}
+
+void bf_run(instruct *p_program)
+{
+	cell *p_curr_cell = initialize_ribbon();
+	instruct *p_curr_instruct = p_program->p_next_instruct;
+
+	while (p_curr_instruct->instruct != 0)
+	{
+		if (p_curr_instruct->instruct == '+')
+		{
+			p_curr_cell->value += 1;
+		}
+		else if (p_curr_instruct->instruct == '-')
+		{
+			p_curr_cell->value -= 1;
+		}
+		else if (p_curr_instruct->instruct == '>')
+		{
+			p_curr_cell = p_curr_cell->p_next_cell;
+		}
+		else if (p_curr_instruct->instruct == '<')
+		{
+			p_curr_cell = p_curr_cell->p_prev_cell;
+		}
+		else if (p_curr_instruct->instruct == '.')
+		{
+			printf("%c", p_curr_cell->value);
+		}
+		else if (p_curr_instruct->instruct == ',')
+		{
+			scanf("%1c");
+		}
+
+		if ((p_curr_instruct->instruct == '[' && p_curr_cell->value == 0) || (p_curr_instruct->instruct == ']' && p_curr_cell->value != 0))
+		{
+			p_curr_instruct = p_curr_instruct->p_matching_instruct;
+		}
+		else
+		{
+			p_curr_instruct = p_curr_instruct->p_next_instruct;
+		}
+	}
+
+	return;
+}
+
+cell *initialize_ribbon()
+{
+	cell *p_first_cell = calloc(sizeof(cell), 1);
+	cell *p_curr_cell = p_first_cell;
+	int iterator;
+
+	p_first_cell;
+	for (iterator = 0; iterator < 1024; iterator++)
+	{
+		p_curr_cell->p_next_cell = calloc(sizeof(cell), 1);
+		p_curr_cell->p_next_cell->p_prev_cell = p_curr_cell;
+		p_curr_cell = p_curr_cell->p_next_cell;
+	}
+	p_curr_cell->p_next_cell = p_first_cell;
+	p_first_cell->p_prev_cell = p_curr_cell;
+
+	return p_first_cell;
 }
